a6332c4c3aa9a547adb5561e97c946f4ab6441b5
Aivars Kalvans <aivars.kalvans@tieto.com>
2021-06-11 23:34:08 +0300

    renamed

--- domains/toupper/toupper.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import sys
-import tuxedo as t
-
-class Server:
-    def TOUPPER(self, req):
-        return t.tpreturn(t.TPSUCCESS, 0, req.upper())
-
-t.run(Server(), sys.argv)
